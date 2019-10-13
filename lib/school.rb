238f0f6296db87ac4a57f15d72d932265eb860db
Darrian Soto <ddog350@gmail.com>
2019-10-13 21:15:26 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,15 @@
-# code here!
+class School
+  attr_reader :roster
+  
+  
+  
+  def initialize(name)
+    @name = name
+    ROSTER = {}
+  end
+  
+  def add_student(grade, student_name)
+    
+  end
+  
+end
